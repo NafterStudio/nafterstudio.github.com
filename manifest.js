03c9a62b9da095270045b8ba90b56b50845b643d
Hotower <31088090+Hotower@users.noreply.github.com>
2022-02-03 20:40:09 +0800

    Update manifest.js
    5 sample passages

--- manifest.js
+++ manifest.js
@@ -14,6 +14,42 @@ let manifest = [
 			"title" : "你好",
 			"RDL" : "h2\\我是谁\\p\\我是热力天蝎，一个蒻得不能再蒻的大蒟蒻！\\p\\<del>尽管学过oi，却菜得和没学过一样！</del>\\h2\\学过的语言\\p\\Scratch罢了\\p\\<del>和同学赌输了再也不碰oi</del>\\h2\\最近在做的\\p\\如你所见\\h2\\如何联系\\p\\QQ:271700283\\",
 			"tag" : ['self']
+		},
+
+		{
+			"key" : "Goodbye",
+			"title" : "再见",
+			"RDL" : "高考难死了。",
+			"tag" : ['journal']
+		},
+
+		{
+			"key" : "tql",
+			"title" : "%%%",
+			"RDL" : "%ls，林湃tql",
+			"tag" : ['journal']
+		},
+
+		{
+			"key" : "csl",
+			"title" : "好菜啊",
+			"RDL" : "[x]\\翻车老菜",
+			"tag" : ['journal']
+		},
+
+		{
+			"key" : "tcl",
+			"title" : "弱",
+			"RDL" : "。。",
+			"tag" : ['journal']
+		} ,
+
+		{
+			"key" : "不对劲烦",
+			"title" : "好的坏小孩",
+			"RDL" : "[]\\和市场经济的好地方很单纯",
+			"tag" : ['journal']
 		}
+
 	]
 ];
